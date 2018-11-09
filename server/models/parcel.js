670c895f0a61c1e72cc6719f90ec258a5cb61a15
kellswork <kellsogbonna@gmail.com>
2018-11-09 20:58:49 +0100

    Add more data to the parcel models
    I added more data to the parcel model for example the userId
    I will use it to retrieve the parcel delivery order each user has created
    I also added more rides just to increase the number of parel delivery order for some users.

--- server/models/parcel.js
+++ server/models/parcel.js
@@ -1,6 +1,7 @@
 const parcel = [
   {
     id: 1,
+    userId: 101,
     name: 'kelechi Ogbonna',
     productName: 'Zaron too faced powder',
     pickupAddress: 'no 2 allen avenue oshodi lagos',
@@ -10,6 +11,7 @@ const parcel = [
   },
   {
     id: 2,
+    userId: 102,
     name: 'sandra eze',
     productName: 'electronic toothbrush',
     pickupAddress: 'no 2 fox road detox avenue apapa lagos',
@@ -19,6 +21,7 @@ const parcel = [
   },
   {
     id: 3,
+    userId: 103,
     name: 'daniel casey',
     productName: 'Iphone-X',
     pickupAddress: 'no 2 fox road detox fallout garrison victoria island lagos',
@@ -28,6 +31,7 @@ const parcel = [
   },
   {
     id: 4,
+    userId: 104,
     name: 'stanley spoon',
     productName: 'aluminium paper',
     pickupAddress: 'no 2 fox road detox avenue apapa lagos',
@@ -36,11 +40,32 @@ const parcel = [
   },
   {
     id: 5,
+    userId: 105,
     name: 'della tabel',
     productName: 'milani lipstick',
     pickupAddress: 'no 2 fox road detox avenue apapa lagos',
     destinationAddress: 'no 2 allen avenue oshodi lagos',
     status: 'in transit',
+
+  },
+  {
+    id: 6,
+    userId: 103,
+    name: 'daniel casey',
+    productName: 'Gucci leather belt',
+    pickupAddress: 'no 2 fox road detox avenue apapa lagos',
+    destinationAddress: 'no 2 allen avenue oshodi lagos',
+    status: 'in transit',
+
+  },
+  {
+    id: 7,
+    userId: 105,
+    name: 'della tabel',
+    productName: 'ring lights',
+    pickupAddress: 'no 2 fox road detox avenue apapa lagos',
+    destinationAddress: 'no 2 allen avenue oshodi lagos',
+    status: 'delivered',
 
   },
 ];
