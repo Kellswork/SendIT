c57724be06c5c3c0abf8ddc87bb30f10146b5c14
kellswork <kellsogbonna@gmail.com>
2018-11-09 20:52:26 +0100

    Create model for users I will us this user model to get all the parcel delivery order each user has created form the parcel model

---
+++ server/models/users.js
@@ -0,0 +1,29 @@
+const users = [
+  {
+    userId: 101,
+    name: 'kelechi Ogbonna',
+    email: 'kellogbonna@gmail.com',
+  },
+  {
+    userId: 102,
+    name: 'sandra eze',
+    email: 'sandraeze@gmail.com',
+  },
+  {
+    userId: 103,
+    name: 'daniel casey',
+    email: 'danielcasey@gmail.com',
+  },
+  {
+    userId: 104,
+    name: 'stanley spoon',
+    email: 'stanleyspoon@gmail.com',
+  },
+  {
+    userId: 105,
+    name: 'della tabel',
+    email: 'dellatabel@gmail.com',
+  },
+];
+
+export default users;
