3cdd4654145f3fa491ed79d6e3e2fbabfecb3fda
kellswork <kellsogbonna@gmail.com>
2018-11-13 13:13:47 +0100

    commit ab5108750d4c17347da7004728e8dbac45b6beb1

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ function openNav() {
  if (nav.style.display === 'block') nav.style.display = 'none';
  else nav.style.display = 'block';
 }
+
+function openNotification() {
+  const displayNotification = document.querySelector('.dropdown-notification');
+ if (displayNotification.style.display === 'block') displayNotification.style.display = 'none';
+ else displayNotification.style.display = 'block';
+}
