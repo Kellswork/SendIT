7672b9f0c6dc3ca82243527341a35ffab08003a8
kellswork <kellsogbonna@gmail.com>
2018-11-15 10:10:28 +0100

    Add return statement to JSON response for conditionals
    I added the return statement to the json response so that the code exits after one condition has been met.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,7 +2,12 @@ import parcel from '../models/parcel';
 
 class Parcelorder {
   static getAllparcelOrders(req, res) {
-    res.status(200).json({
+    if (!parcel || parcel === null) {
+      return res.status(404).json({
+        message: 'No parcel delivery order has been created',
+      });
+    }
+    return res.status(200).json({
       message: ' Parcel orders',
       parcel,
     });
@@ -10,13 +15,11 @@ class Parcelorder {
 
   static getOneParcelOrder(req, res) {
     const oneParcel = parcel.find(x => x.id === parseInt(req.params.id, 10));
-    if (!oneParcel) res.status(404).json('Parcel does not exist');
-    else {
-      res.status(200).json({
-        message: 'parcel order',
-        Details: oneParcel,
-      });
-    }
+    if (!oneParcel) return res.status(404).json('Parcel does not exist');
+    return res.status(200).json({
+      message: 'parcel order',
+      Details: oneParcel,
+    });
   }
 
   static createParcelOrder(req, res) {
@@ -37,9 +40,9 @@ class Parcelorder {
   static cancelParcelOrder(req, res) {
     const oneParcel = parcel.find(x => x.id === parseInt(req.params.id, 10));
     if (!oneParcel) return res.status(404).json('Parcel does not exist');
-    oneParcel.status = 'canceled';
+    oneParcel.status = 'cancelled';
     return res.status(200).json({
-      message: 'Your parcel delivery order has been canceled',
+      message: 'Your parcel delivery order has been cancelled',
       Details: oneParcel,
     });
   }
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -3,19 +3,22 @@ import parcel from '../models/parcel';
 
 class User {
   static getAllUsers(req, res) {
-    res.status(200).send(user);
+    if (!user || user === null) {
+      return res.json({
+        message: 'No user found',
+      });
+    }
+    return res.status(200).json(user);
   }
 
   static getAllParcelOrderCreatedByUser(req, res) {
     const userParcel = parcel.filter(x => x.userId === parseInt(req.params.userId, 10));
     // get all parcel orders with the userId
-    if (!userParcel) res.status(404).json('Parcel does not exist');
-    else {
-      res.status(200).json({
-        message: 'parcel orders you have created',
-        Details: userParcel,
-      });
-    }
+    if (!userParcel) return res.status(404).json('Parcel does not exist');
+    return res.status(200).json({
+      message: 'parcel orders you have created',
+      Details: userParcel,
+    });
   }
 }
 
