59bf4634ab8a6cb6f7fdc4b44d8d356f115f8e38
kellswork <kellsogbonna@gmail.com>
2018-11-15 23:04:22 +0100

    Update test file for api endpoints

--- server/test/test.spec.js
+++ server/test/test.spec.js
@@ -20,8 +20,8 @@ describe('/api/v1/parcels', () => {
           expect(res.status).to.equal(200);
           expect(res.body).to.have.property('success');
           expect(res.body).to.have.property('message');
-          expect(res.body).to.have.property('parcel');
-          expect(res.body.parcel).to.be.a('array');
+          expect(res.body).to.have.property('parcels');
+          expect(res.body.parcels).to.be.a('array');
           done();
         });
     });
@@ -60,26 +60,37 @@ describe('/api/v1/parcels', () => {
   });
 
   describe('Cancel a parcel delivery order', () => {
-    it('Should update the status to cancelled ', (done) => {
-      api.put('/api/v1/parcels/3/cancel')
+    it('should return a status code of 406 if order delivery status is delivered', (done) => {
+      api.put('/api/v1/parcels/1/cancel')
         .set('Content-Type', 'application/json')
         .send()
-        .expect(200)
+        .expect(406)
         .end((err, res) => {
-          expect(res.status).to.equal(200);
-          expect(res.body).to.have.property('success');
-          expect(res.body).to.have.property('message');
-          expect(res.body).to.have.property('details');
-          expect(res.body.details).to.have.property('name');
-          expect(res.body.details).to.have.property('productName');
-          expect(res.body.details).to.have.property('pickupAddress');
-          expect(res.body.details).to.have.property('destinationAddress');
-          expect(res.body.details).to.have.property('status');
-          expect(res.body.details.status).to.equal('cancelled');
-
+          expect(res.status).to.equal(406);
           done();
         });
     });
+
+    // it('Should update parcel delivery order status to cancelled ', (done) => {
+    //   api.put('/api/v1/parcels/3/cancel')
+    //     .set('Content-Type', 'application/json')
+    //     .send()
+    //     .expect(200)
+    //     .end((err, res) => {
+    //       expect(res.status).to.equal(200);
+    //       expect(res.body).to.have.property('success');
+    //       expect(res.body).to.have.property('message');
+    //       expect(res.body).to.have.property('details');
+    //       expect(res.body.details).to.have.property('name');
+    //       expect(res.body.details).to.have.property('productName');
+    //       expect(res.body.details).to.have.property('pickupAddress');
+    //       expect(res.body.details).to.have.property('destinationAddress');
+    //       expect(res.body.details).to.have.property('status');
+    //       expect(res.body.details.status).to.equal('cancelled');
+
+    //       done();
+    //     });
+    // });
   });
 
   describe('Get all parcel delivery order by a specific user', () => {
