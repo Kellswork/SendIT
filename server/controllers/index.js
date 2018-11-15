9428c0120a0bc7af8674cd2571a5048c4eebf796
kellswork <kellsogbonna@gmail.com>
2018-11-15 10:44:48 +0100

    Add success to returned JSON response
    Success is only true when the status code is either 201 or 200

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -4,20 +4,28 @@ class Parcelorder {
   static getAllparcelOrders(req, res) {
     if (!parcel || parcel === null) {
       return res.status(404).json({
-        message: 'No parcel delivery order has been created',
+        Success: false,
+        Message: 'No parcel delivery order has been created',
       });
     }
     return res.status(200).json({
-      message: ' Parcel orders',
+      Success: true,
+      Message: 'Parcel delivery orders',
       parcel,
     });
   }
 
   static getOneParcelOrder(req, res) {
     const oneParcel = parcel.find(x => x.id === parseInt(req.params.id, 10));
-    if (!oneParcel) return res.status(404).json('Parcel does not exist');
+    if (!oneParcel) {
+      return res.status(404).json({
+        Success: false,
+        Message: 'Parcel delivery order does not exist',
+      });
+    }
     return res.status(200).json({
-      message: 'parcel order',
+      Success: true,
+      Message: 'Parcel delivery order details',
       Details: oneParcel,
     });
   }
@@ -32,16 +40,23 @@ class Parcelorder {
     };
     parcel.push(newParcel);
     res.status(201).json({
-      message: 'Parcel order successfully created',
+      Success: true,
+      Message: 'Parcel delivery order created successfully',
       Details: newParcel,
     });
   }
 
   static cancelParcelOrder(req, res) {
     const oneParcel = parcel.find(x => x.id === parseInt(req.params.id, 10));
-    if (!oneParcel) return res.status(404).json('Parcel does not exist');
+    if (!oneParcel) {
+      return res.status(404).json({
+        Success: false,
+        Message: 'Parcel does not exist',
+      });
+    }
     oneParcel.status = 'cancelled';
     return res.status(200).json({
+      Success: true,
       message: 'Your parcel delivery order has been cancelled',
       Details: oneParcel,
     });
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -5,6 +5,7 @@ class User {
   static getAllUsers(req, res) {
     if (!user || user === null) {
       return res.json({
+        Success: false,
         message: 'No user found',
       });
     }
@@ -14,9 +15,10 @@ class User {
   static getAllParcelOrderCreatedByUser(req, res) {
     const userParcel = parcel.filter(x => x.userId === parseInt(req.params.userId, 10));
     // get all parcel orders with the userId
-    if (!userParcel) return res.status(404).json('Parcel does not exist');
+    if (!userParcel || userParcel === null) return res.status(404).json('You have not created any parcel delivery order');
     return res.status(200).json({
-      message: 'parcel orders you have created',
+      Success: true,
+      message: 'Parcel delivery orders you have created',
       Details: userParcel,
     });
   }
