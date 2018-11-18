f3411f564abd7232eafc7674e76750f97f07701d
kellswork <kellsogbonna@gmail.com>
2018-11-18 15:39:30 +0100

    Get all validation error message

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import route from './routes/index';
+import parcel from './routes/parcels';
 import user from './routes/users';
 import logger from './config/winston';
+
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.get('/', (req, res) => res.status(200).json({
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/api/v1/parcels', route);
+app.use('/api/v1/parcels', parcel);
 app.use('/api/v1/users', user);
 app.use((req, res, next) => {
   const error = new Error('Could not find page with this address');
