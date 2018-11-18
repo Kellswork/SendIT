f37acda7a02a7aaf47ceb134c202eb8b1de98885
kellswork <kellsogbonna@gmail.com>
2018-11-18 14:23:00 +0100

    Add change address feature to order details page
    I added a box that displays when the user clicks on change address.
    I also maade the pages responsive and added a mobile nav

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const displayChangeDestination = document.querySelector('.modal');
+
+
 function openNav() {
   const nav = document.getElementById('mobileNav');
  if (nav.style.display === 'block') nav.style.display = 'none';
@@ -9,3 +12,26 @@ function openNotification() {
  if (displayNotification.style.display === 'block') displayNotification.style.display = 'none';
  else displayNotification.style.display = 'block';
 }
+
+function openChangeDestination() {
+ if (displayChangeDestination.style.display === 'block') displayChangeDestination.style.display = 'none';
+ else displayChangeDestination.style.display = 'block';
+}
+
+function closeChangeDestinationBox(){
+ displayChangeDestination.style.display = 'none';
+
+}
+function openMobileNav() {
+  const mobileNav = document.querySelector('.mobile-nav');
+  if (mobileNav.style.display === 'block') mobileNav.style.display = 'none';
+ else mobileNav.style.display = 'block';
+}
+
+document.querySelector('.profile-img').addEventListener('click', openMobileNav);
+
+document.querySelector('.close').addEventListener('click', closeChangeDestinationBox)
+const changeDestination = document.querySelector('.destination-info-change');
+
+changeDestination.addEventListener('click', openChangeDestination);
+
