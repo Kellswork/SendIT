c1b554aa3028a9b19f55af40481bb931caa28cff
kellswork <kellsogbonna@gmail.com>
2018-11-22 19:12:58 +0100

    --amend

--- server/models/connect.js
+++ server/models/connect.js
@@ -35,9 +35,7 @@ export default db;
 */
 const { Client } = require('pg');
 
-class Database {
-  constructor() {
-    this.createUserTableQuery = `CREATE TABLE IF NOT EXISTS users(
+    const createUserTableQuery = `CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY, 
     firstname VARCHAR(50) NOT NULL, 
     lastname VARCHAR(50) NOT NULL,
@@ -48,7 +46,7 @@ class Database {
     is_admin BOOLEAN DEFAULT FALSE,
     registered_on DATE DEFAULT CURRENT_TIMESTAMP)`;
 
-    this.createParcelTableQuery = `CREATE TABLE IF NOT EXISTS 
+    const createParcelTableQuery = `CREATE TABLE IF NOT EXISTS 
     parcels(id SERIAL PRIMARY KEY, 
     placed_by INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     weight NUMERIC NOT NULL,
@@ -63,36 +61,37 @@ class Database {
     status VARCHAR(20) DEFAULT 'placed',
     current_location TEXT)`;
     // console.log(Database.instance)
-    this.connect();
-  }
+  
 
-  connect() {
-    if (!Database.instance) {
-      const connectionString = process.env.DATABASE_URL || 'postgresql://kells:kells123@localhost:5432/sendit';
-      const instance = new Client({ connectionString });
-       console.log(instance);
-      instance.connect();
-       instance.query(this.createUserTableQuery);
-       instance.query(this.createParcelTableQuery);
-      Database.instance = instance;//
-    }
-  }
+      const connectionString = process.env.DATABASE_URL;
+      /*|| 'postgresql://kells:kells123@localhost:5432/sendit'; */
 
+      let ins = new Client({ connectionString });
+      ins.connect();
+
+      let db = {
+        query: (text, params) => ins.query(text, params),
+      };
+      // instance.query(this.createUserTableQuery);
+     //  instance.query(this.createParcelTableQuery);
+
+
+/*
   async query(query) {
     const result = await Database.instance.query(query);
     return result;
   }
+  */
 
-  dropUsers() {
+  function dropUsers() {
     this.query('DROP TABLE users CASCADE');
   }
 
-  dropParcels() {
+  function dropParcels() {
     this.query('DROP TABLE parcels');
   }
-}
+
 
 
 // // test();
-const db = new Database();
  export default db;
