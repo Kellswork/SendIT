1655c7fc0122c4e55819003713a1259958486e19
kellswork <kellsogbonna@gmail.com>
2018-11-22 10:54:03 +0100

    Add seeds to database

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,6 @@ module.exports = {
     "extends": "airbnb-base",
     "rules" : {
         "consistent-return" : 0,
-        "class-methods-use-this" : 0
+        "class-methods-use-this" : 0,
     }
 };
